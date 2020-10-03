ab8f9edad44ac063313667df2f99951042d8ef5a
matias.luna <matias.luna@codetria.com>
2020-10-03 16:12:25 -0300

    Se agrega carpeta de tests con un dummy test. Se borra el defaulpackage con el main.

---
+++ tests/loveletter/RondaTest.java
@@ -0,0 +1,24 @@
+package loveletter;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class RondaTest {
+
+
+	@Before
+	public void setUp() {
+	
+	}
+	
+	@Test
+	public void dummyTest() {
+		assertEquals(1,1);
+	}
+	
+
+}
