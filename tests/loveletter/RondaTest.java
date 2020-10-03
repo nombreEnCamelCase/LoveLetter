1271abf07e2d2b5b43e6fc1ab43df7a049126e8a
matias.luna <matias.luna@codetria.com>
2020-10-03 16:43:48 -0300

    Cambios en ronda test.

--- tests/loveletter/RondaTest.java
+++ tests/loveletter/RondaTest.java
@@ -1,7 +1,7 @@
 package loveletter;
 
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -18,7 +18,5 @@ public class RondaTest {
 	@Test
 	public void dummyTest() {
 		assertEquals(1,1);
-	}
-	
-
+	} 
 }
