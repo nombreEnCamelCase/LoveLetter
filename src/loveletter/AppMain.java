9e2c1627329f5374ac99ffb311c6239206642f0f
federacing94 <federicovernet@gmail.com>
2020-10-04 19:26:47 -0300

    mazo

---
+++ src/loveletter/AppMain.java
@@ -0,0 +1,9 @@
+package loveletter;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		Mazo mazo = new Mazo();
+	}
+
+}
--- src/loveletter/Mazo.java
+++ src/loveletter/Mazo.java
@@ -3,8 +3,27 @@ package loveletter;
 import java.util.ArrayList;
 
 public class Mazo {
-	
+
 	private int cantidad;
 	private ArrayList<Carta> carta;
+
+	public Mazo() {
+		carta.add(new Baron());
+		carta.add(new Baron());
+		carta.add(new Sacerdote());
+		carta.add(new Sacerdote());
+		carta.add(new Mucama());
+		carta.add(new Mucama());
+		carta.add(new Principe());
+		carta.add(new Rey());
+		carta.add(new Condesa());
+		carta.add(new Princesa());
+		carta.add(new Guardia());
+		carta.add(new Guardia());
+		carta.add(new Guardia());
+		carta.add(new Guardia());
+		carta.add(new Guardia());
+
+	}
 
 }
