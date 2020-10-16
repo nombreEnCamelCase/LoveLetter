1be2ba9d361d43d734e2550a5ebd69bf61a8292e
federacing94 <federicovernet@gmail.com>
2020-10-16 19:54:35 -0300

    package para cartas tests

---
+++ tests/loveletter/CartasTests/MucamaTests.java
@@ -0,0 +1,14 @@
+package loveletter.CartasTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MucamaTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
