dcecd4d533f8b679e4bcefb7578ab84689206695
Mel-A <meabuin@alumno.unlam.edu.ar>
2020-10-17 13:31:28 -0300

    agrego mas tests a las cartas

--- tests/loveletter/CartasTests/PrincesaTests.java
+++ tests/loveletter/CartasTests/PrincesaTests.java
@@ -2,17 +2,22 @@ package loveletter.CartasTests;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import loveletter.Jugador;
 import loveletter.Cartas.Princesa;
+import loveletter.EstadosJugador.FueraDeRonda;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class PrincesaTests {
 	private Princesa princesa;
+	private Jugador jugador, contrincante;
 
 	@Before
 	public void setUp() {
 		princesa = new Princesa();
+		jugador = new Jugador("princesa");
+		contrincante = new Jugador("contrincante");
 	}
 	
 	@Test
@@ -30,5 +35,11 @@ public class PrincesaTests {
 	@Test
 	public void requiereJugadorAccionadorTests() {
 		assertFalse(princesa.requiereJugadorAccionador());
+	}
+	
+	@Test
+	public void aplicarEfectoAJugadorTests() {
+		princesa.aplicarEfectoAJugador(jugador,contrincante);
+		assertEquals(new FueraDeRonda(), jugador.getEstadoActual());
 	}
 }
