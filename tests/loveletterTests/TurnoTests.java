18e03d73971eee3fd2e4ae293f745801343f178d
KevinGarcia <kevinx-10@hotmail.com>
2020-11-06 20:41:37 -0300

    modifico un poco en turno test

--- tests/loveletterTests/TurnoTests.java
+++ tests/loveletterTests/TurnoTests.java
@@ -3,6 +3,8 @@ package loveletterTests;
 import static org.junit.Assert.*;
 import loveletter.Jugador;
 import loveletter.Mazo;
+import loveletter.Partida;
+import loveletter.Ronda;
 import loveletter.Turno;
 import loveletter.Cartas.Guardia;
 
@@ -16,22 +18,18 @@ public class TurnoTests {
 	@Before
 	public void setUp() {
 		jugador = new Jugador("nombre de jugador");
-		turno = new Turno(jugador);
+		turno = new Turno(jugador);	
 	}
 
-	// 1)PUEDO PROBAR SI EN VERDAD LE PASO JUGADOR Y CARTA
-	// 2)PRUEBO SI RESPONSABLE TOMA BIEN EL JUGADOR
-	// 3)PRUEBO SI CARTA JUGADA TOMA BIEN LA CARTA
-	
 	@Test
 	public void turnoTest() {
-		assertEquals(jugador,turno.getResponsable());
+		assertEquals(jugador, turno.getResponsable());
 	}
-	
+
 	@Test
 	public void setCartaJugadaTest() {
-		
-		assertEquals(null,turno.getCartaJugada());
-		
+		assertEquals(null, turno.getCartaJugada());
 	}
+	
+
 }
